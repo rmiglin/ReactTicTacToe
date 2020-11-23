dadb4a444f9d2e8db7394076193289f07732c0fe
rmiglin <rossmiglin@gmail.com>
2020-11-23 01:36:21 -0500

    move state management from square to board

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -2,28 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }    
-
+class Square extends React.Component {   
     render() {
         return (
-            <button className="square" 
-            onClick={() => this.setState({value: 'X'})}
+            <button 
+                className="square" 
+                onClick={() => this.props.onClick()}
             >
-                {this.state.value}
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            square: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square 
+                value={this.state.squares[i]} 
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
